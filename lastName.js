1db5438729bc79017b4e89795f90bb9b6934444d
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-04-06 01:28:54 +0530

    Enter valid last name

---
+++ lastName.js
@@ -0,0 +1,17 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Please enter your last name', lastName =>{
+    let regex = /^[A-Z][a-zA-Z]{2}/
+    let verify = regex.test(lastName);
+
+    if(verify == true){
+        console.log("Last Name is : "+lastName);
+    }
+    else{
+        console.log("!! Error !! \Last name should contain first capital letter with menimum lenth of 3 characters");
+    }
+    readline.close();
+})
